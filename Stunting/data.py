714f03967a85b1b75fe4a2db9d344ad40d793984
hudaEMY <142593439+hudaEMY@users.noreply.github.com>
2023-12-08 17:36:22 +0700

    Update data.py

--- Stunting/data.py
+++ Stunting/data.py
@@ -19,12 +19,36 @@ for _ in range(1000000):
     panjang = round(random.uniform(30.0, 70.0), 2)
     lingkar_kepala = round(random.uniform(20.0, 50.0), 2)
 
-    selected_data = []
-
     with open(file_path_input, 'r') as file:
         csvreader = csv.DictReader(file)
         for row in csvreader:
             if row['umur'] == str(umur_bulan):
                 data_berat = [float(row['batas bawah berat']), float(row['batas atas berat'])]
                 data_panjang = [float(row['batas bawah panjang']), float(row['batas atas panjang'])]
-                data_kepala = [float(row['batas bawah kepala']), float(row[
+                data_kepala = [float(row['batas bawah kepala']), float(row['batas atas kepala'])]
+
+                if is_stunted(berat, panjang, lingkar_kepala, data_berat, data_panjang, data_kepala):
+                    result = "stunting"
+                else:
+                    result = "tidak stunting"
+
+                selected_data = {
+                    'jenis_kelamin': jenis_kelamin,
+                    'umur': umur_bulan,
+                    'berat': berat,
+                    'panjang': panjang,
+                    'kepala': lingkar_kepala,
+                    'hasil_stunting': result
+                }
+                random_entries.append(selected_data)
+                break
+
+fieldnames = ['jenis_kelamin', 'umur', 'berat', 'panjang', 'kepala', 'hasil_stunting']
+
+with open(file_path_output, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for data in random_entries:
+        writer.writerow(data)
+
+print("Data has been saved to data_train.csv.")
