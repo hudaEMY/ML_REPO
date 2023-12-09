1c00db98d4775ee788cf6cca46423a52e9cb8571
hudaEMY <142593439+hudaEMY@users.noreply.github.com>
2023-12-09 22:40:16 +0700

    Add files via upload

--- Stunting/stunting.tf.py
+++ Stunting/stunting.tf.py
@@ -4,20 +4,21 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras import regularizers
 from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras import regularizers
+from sklearn.metrics import classification_report, roc_auc_score
 
-file_path = r'D:\CAPESTONE\Stunting\data_train.csv'
+file_path = r'D:\CAPESTONE\Stunting\dataset_acak.csv'
 data = pd.read_csv(file_path)
 
 data = data.dropna()
 
 label_encoder = LabelEncoder()
-data['jenis_kelamin'] = label_encoder.fit_transform(data['jenis_kelamin'])
-data['hasil_stunting'] = label_encoder.fit_transform(data['hasil_stunting'])
+data['kelamin'] = label_encoder.fit_transform(data['kelamin'])
+data['label'] = label_encoder.fit_transform(data['label'])
 
-X = data[['jenis_kelamin', 'umur', 'berat', 'panjang', 'kepala']]
-y = data['hasil_stunting']
+X = data[['kelamin', 'umur', 'berat', 'panjang', 'kepala']]
+y = data['label']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -33,13 +34,22 @@ model = Sequential([
     Dense(1, activation='sigmoid')
 ])
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+early_stopping = EarlyStopping(monitor='val_loss', patience=1000  )
 
-model.fit(X_train_scaled, y_train, epochs=3, batch_size=32, validation_split=0.2, callbacks=[early_stopping])
+model.fit(X_train_scaled, y_train, epochs=50000, batch_size=32, validation_split=0.2, callbacks=[early_stopping])
 
 model.save('stunting.h5')
 
 test_loss, test_accuracy = model.evaluate(X_test_scaled, y_test)
 print(f"Test Accuracy: {test_accuracy}")
+
+y_pred = model.predict(X_test_scaled)
+y_pred_binary = (y_pred > 0.5).astype(int)
+
+print("Classification Report:")
+print(classification_report(y_test, y_pred_binary))
+
+print("AUC-ROC Score:", roc_auc_score(y_test, y_pred))
